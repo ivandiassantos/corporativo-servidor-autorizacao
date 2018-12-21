cba1f39adce8edd642e2a1b3a79a17f3366802f9
Ivan <ivan.dias.santos@gmail.com>
2018-12-20 23:49:25 -0200

    Correção no módulo de autorização

--- src/main/java/br/com/servidor/autorizacao/ServidorAutorizacaoApplication.java
+++ src/main/java/br/com/servidor/autorizacao/ServidorAutorizacaoApplication.java
@@ -2,10 +2,10 @@ package br.com.servidor.autorizacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ServidorAutorizacaoApplication {
 
 	public static void main(String[] args) {
